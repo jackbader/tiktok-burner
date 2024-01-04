0c17afc9083ceaa573211d7bc9a89b3fdca7e5b3
Jack <jack@culpritunderwear.com>
2024-01-04 14:11:49 -0700

    og tags

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <script setup>
 import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+
+useHead({
+  title: "My App",
+  meta: [
+    {
+      name: "description",
+      content: "Creating colorful pieces to brighten your world",
+    },
+    { name: "og:url", content: "https://kellybaderart.com" },
+    { name: "og:title", content: "KellyBaderArt" },
+    {
+      name: "og:description",
+      content: "Creating colorful pieces to brighten your world",
+    },
+    {
+      name: "og:image",
+      content: "https://www.kellybaderart.com/imgs/portrait.png",
+    },
+  ],
+});
 </script>
 
 <template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,34 @@
 <script>
 import "assets/css/main.css";
+export default {
+  head: {
+    title: "KellyBaderArt",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Creating colorful pieces to brighten your world",
+      },
+      { hid: "og:title", property: "og:title", content: "KellyBaderArt" },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "Creating colorful pieces to brighten your world",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "	https://www.kellybaderart.com/imgs/portrait.png",
+      },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://kellybaderart.com",
+      },
+      // any other OG tags you want to add
+    ],
+  },
+};
 </script>
 
 <template>
