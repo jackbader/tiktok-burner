ae4d43d44823842213537ec8602295db1ab74b81
Jack <jack@culpritunderwear.com>
2024-01-05 15:02:29 -0500

    updating about page

--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,26 @@
       class="md:w-1/2 w-full mb-8 text-center flex flex-col items-center"
     >
       <h1 class="text-4xl font-bold mb-4">About Me</h1>
-      <p class="text-lg max-w-xl">ect ect ect ect...</p>
+      <p class="text-sm max-w-xl">
+        Kelly grew up in a very country setting in Ohio. Her and Huck Finn
+        would’ve been best friends! She loved the river, the woods, exploring
+        every day and finding treasures wherever her day took her! She comes
+        from a family of artists; her mom gave her the love of art and it was
+        always in the back of her mind somewhere hiding.<br /><br />
+        Fast forward to 2018 and that’s when she decided to pick up a paintbrush
+        for the very first time! No real formal training, she decided to enroll
+        in a local painting class and also enrolled in online classes to hone
+        her skills. She gets inspiration for her paintings by just observing
+        what’s around her in nature, her travels, people she’s met and people
+        she admires. Painting women is her favorite subject. She has always been
+        attracted to bright, bold colors and you can view that in most of her
+        paintings. <br />
+        <br />
+        Kelly has lived in Boulder County for over 30 years. She lives with her
+        husband and her ornery rescue dog, Max and her princess, Penelope, a
+        King Charles Cavalier. She loves hiking everyday with her dogs, curling
+        up with a book and binging the latest shows with her husband.
+      </p>
     </UContainer>
   </div>
 </template>
